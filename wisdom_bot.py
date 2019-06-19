deb2365eaf3536cd3669f13b386511abdbbe977a
mongolianjesus <mongolianjesus@github.com>
2019-06-19 04:16:42 +0300

    fixed flask bugs

--- wisdom_bot.py
+++ wisdom_bot.py
@@ -54,5 +54,7 @@ def webhook():
         flask.abort(403)
 
 if __name__ == '__main__':
+    import os
     app.debug = True
-    app.run(host=conf.WEBHOOK_HOST, port=conf.WEBHOOK_PORT)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
