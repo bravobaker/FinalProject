7c0c783a59b34d1c5d9fc45e44ac17f6af1f9323
mongolianjesus <mongolianjesus@github.com>
2019-06-19 05:02:56 +0300

    fix

--- wisdom_bot.py
+++ wisdom_bot.py
@@ -41,7 +41,7 @@ def scorn_insolence(message):
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
     return "!", 200
 
 @app.route("/")
